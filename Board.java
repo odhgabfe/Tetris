4d7173f8743b30bde7918e8a738be274e3e0b6b3
Miko <mikoto.uwu@gmail.com>
2019-02-17 13:56:19 +0530

    set up the background grid

--- Board.java
+++ Board.java
@@ -8,6 +8,11 @@ import javax.swing.JPanel;
 public class Board extends JPanel {
 
     private BufferedImage blocks;
+    private final int blockSize = 30;
+    private final int boardWidth = 10;
+    private final int boardHeight = 20;
+
+    private int[][] board = new int[boardWidth][boardHeight];
 
     public Board() {
         try {
@@ -19,8 +24,15 @@ public class Board extends JPanel {
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g); //call parent JPanel's implementation of the method paintComponent on g
-        g.draw3DRect(100, 100, 50, 50, true);
-        
-        g.drawImage(blocks, 0, 0, null);
+
+        for (int i = 0; i < boardHeight; i++) {
+            //horizontal lines
+            g.drawLine(0, i * blockSize, boardWidth * blockSize, i * blockSize);
+        }
+
+        for (int i = 0; i < boardWidth; i++) {
+            //vertical lines
+            g.drawLine(i * blockSize, 0, i * blockSize, boardHeight * blockSize);
+        }
     }
 }
